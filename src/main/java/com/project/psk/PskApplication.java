3e89559f1ad64798e54bdd7fcea89c2fc319eff3
Deniza <denizaalijeva@gmail.com>
2024-04-16 16:50:53 +0300

    initial commit

---
+++ src/main/java/com/project/psk/PskApplication.java
@@ -0,0 +1,13 @@
+package com.project.psk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PskApplication.class, args);
+	}
+
+}
